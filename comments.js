b024d11767f1449243ea499f9b2156b25bae5d7f
PavelPaha <69955792+PavelPaha@users.noreply.github.com>
2020-10-17 11:50:19 +0500

    img1

---
+++ comments.js
@@ -0,0 +1,7 @@
+// let form = document.querySelector('form');
+
+let Button = document.querySelector('.send>input');
+
+submitButton.onsubmit = function(){
+	console.log('1');
+};
--- script.js
+++ script.js
@@ -1,63 +1,55 @@
-let posts = document.querySelectorAll('.main-flex-content-item');
-let bigBlock1 = document.querySelectorAll('.big-block1');
-let bigBlock2 = document.querySelectorAll('.big-block2');
-// let img = document.querySelectorAll('img');
-let MainFlexContent = document.querySelectorAll('.main-flex-content');
-let select = document.querySelector('#select1');
-let city = document.querySelector('.city');
-let books = document.querySelector('.books');
-let journey = document.querySelector('.journey');
+let form = document.querySelector('form');
+let send = document.querySelector('.send');
+let text = document.querySelector('textarea');
+let submitButton = document.querySelector('.send>button');
+let name = document.querySelector('form .name');
+let email = document.querySelector('form').email;
+let comm = document.querySelector('.orientation-comments');
+let trueAddress = /[0-9a-z_]+@[0-9a-z_]+\.[a-z]{2,5}/i;
+let i1 = false;
+let i2 = false;
+let i3 = false;
+function valid(form){
+	
+	if (name == "" || name == " "){
+		alert('Вы не ввели своё имя');
 
-
-select.onchange = function(){
-	console.log(select.value);
-	for (let post of posts){
-		post.classList.add('none');
 	}
-	for (let block1 of bigBlock1){
-		block1.classList.add('none');
-	}
-	for (let block2 of bigBlock2){
-		block2.classList.add('none');
-	}
-
-
-
-	if (select.value == 'recipes'){
-		for (let block2 of bigBlock2){
-			block2.classList.remove('none');
-		}
-	}
-	if (select.value == 'photos'){
-		for (let block1 of bigBlock1){
-		block1.classList.remove('none');
-		}
-	}
-
-	if (select.value == 'city'){
-		city.classList.remove('none');
-	}
-
-	if (select.value == 'journey'){
-		journey.classList.remove('none');
-	}
-
-	if (select.value == 'books'){
-		books.classList.remove('none');
-	}
-
-	if (select.value == 'all') {
-		for (let post of posts){
-		post.classList.remove('none');
-	}
-	for (let block1 of bigBlock1){
-		block1.classList.remove('none');
-	}
-	for (let block2 of bigBlock2){
-		block2.classList.remove('none');
-	}
+	if (email == ""){
+		alert("Вы не ввели эл. почту");
 	}
 }
 
+text.oninput = function(){
+	text.style.color = '#202117';
+	text.style.borderColor = 'black';
+	i1 = true;
+	if (text.value.length < 10 || text.value.length > 200){
+		text.style.color = 'red';
+		text.style.borderColor = 'red';
+		i1 = false;
+	}
+	if (i1 && i2 && i3) submitButton.disabled = false;
+}
+name.oninput = function(){
+	i2=true;
+	text.style.color = '#202117';
+	text.style.borderColor = 'black';
+	if (name.value.length==" " || name.value.length==" "){
+		i2=false;
+	}
+	if (i1 && i2 && i3) submitButton.disabled = false;
+}
+email.oninput = function(){
+	i3 = true
+	if (trueAddress.test(email) == false){
+		i3 = false;
+	}
+	if (i1 && i2 && i3) submitButton.disabled = false;
+}
+
+submitButton.onclick = function(){
+	comm.createElement('div.comment');
 
 
+}
