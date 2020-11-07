504c98340d6e53ce1c25b6a73ba60b824707f2aa
PavelPaha <vasilievpavel4@gmaol.com>
2020-11-07 11:21:29 +0500

    3dcard
    3dcard

--- 3dcard.js
+++ 3dcard.js
@@ -1,10 +1,17 @@
-const card = document.querySelector(".card");
+const card = document.querySelectorAll(".card");
+for (let i of card){
+	i.addEventListener("mousemove", startRotate);
+	i.addEventListener("mouseout", stopRotate);
+}
 
-card.addEventListener("mousemove", rotate)
 
-
-function rotate(event){
+function startRotate(event){
 	const cardItem = this.querySelector(".card__item");
 	const halfHeight = cardItem.offsetHeight / 2;
-	cardItem.style.transform = 'rotateX('+ - (event.offsetY - halfHeight)/5 + 'deg)';
+	cardItem.style.transform = 'rotateX('+ - (event.offsetY - halfHeight)/7 + 'deg) rotateY('+(event.offsetX - halfHeight)/7 + 'deg) ';
+}
+
+function stopRotate(event){
+	const cardItem = this.querySelector(".card__item");
+	cardItem.style.transform = 'rotate(0)';
 }
